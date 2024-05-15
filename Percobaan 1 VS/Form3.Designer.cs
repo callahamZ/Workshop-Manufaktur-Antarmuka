4e43936de6d4e7eb545609ecca3efb2490094e44
Ilham Kurniawan <ilhamkurniawan1945@gmail.com>
2024-05-15 20:00:04 +0700

    Mengubah font color aplikasi COM

--- Percobaan 1 VS/Form3.Designer.cs
+++ Percobaan 1 VS/Form3.Designer.cs
@@ -38,7 +38,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // label1
@@ -123,21 +123,24 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // listBox1
+            // listView1
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(43, 172);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(363, 164);
-            this.listBox1.TabIndex = 13;
+            this.listView1.Alignment = System.Windows.Forms.ListViewAlignment.Left;
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(43, 173);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(363, 164);
+            this.listView1.TabIndex = 13;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(449, 431);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
@@ -167,6 +170,6 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListView listView1;
     }
 }
--- Percobaan 1 VS/Form3.cs
+++ Percobaan 1 VS/Form3.cs
@@ -25,6 +25,7 @@ namespace Percobaan_1_VS
             button1.Enabled = true;
             button2.Enabled = false;
             button3.Enabled = false;
+            comboBox1.ForeColor = Color.Black;
         }
 
         private void Form3_Load(object sender, EventArgs e)
@@ -48,10 +49,12 @@ namespace Percobaan_1_VS
             try
             {
                 serialPort1.Open();
+                comboBox1.ForeColor = Color.Green;
                 MessageBox.Show(serialPort1.PortName + " tersambung!", "Pemberitahuan", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 button1.Enabled = false;
                 button2.Enabled = true;
                 button3.Enabled = true;
+                
             }
             catch (Exception ex)
             {
@@ -62,21 +65,28 @@ namespace Percobaan_1_VS
         private void button3_Click(object sender, EventArgs e)
         {
             serialPort1.Write(textBox1.Text);
+            listView1.Items.Add(textBox1.Text);
+            listView1.Items[listView1.Items.Count - 1].ForeColor = Color.Magenta;
         }
         private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
         {
             Tampilkan(serialPort1.ReadExisting());
         }
         
-        private delegate void TampilkanDelegate(object item);
-        private void Tampilkan(object item)
+        private delegate void TampilkanDelegate(string item);
+        private void Tampilkan(string item)
         {
             if (InvokeRequired)
+            {
                 // This is a worker thread so delegate the task. 
-                listBox1.Invoke(new TampilkanDelegate(Tampilkan), item);
+                listView1.Invoke(new TampilkanDelegate(Tampilkan), item);
+            }
             else
+            {
                 // This is the UI thread so perform the task. 
-                listBox1.Items.Add(item);
+                listView1.Items.Add(item);
+                listView1.Items[listView1.Items.Count -1].ForeColor = Color.Blue;
+            }
         }
 
         private void Form3_FormClosing(object sender, FormClosingEventArgs e)
